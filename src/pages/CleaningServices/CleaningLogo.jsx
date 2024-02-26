36991b038b1c3bf85143670dd45fbc50ad1cacdd
SeunSarumi <seunsarumi@gmail.com>
2024-02-26 18:45:29 +0100

    cleaning navigation built

---
+++ src/pages/CleaningServices/CleaningLogo.jsx
@@ -0,0 +1,5 @@
+function CleaningLogo() {
+  return <img src="/images/cleaning-logo.png" alt="" />;
+}
+
+export default CleaningLogo;
--- src/pages/CleaningServices/CleaningNav.jsx
+++ src/pages/CleaningServices/CleaningNav.jsx
@@ -1,7 +1,29 @@
+import CleaningLogo from "./CleaningLogo";
 import styles from "./CleaningNav.module.css";
 
 function CleaningNav() {
-  return <div>Navigation</div>;
+  return (
+    <div className={styles.navBody}>
+      <CleaningLogo />
+      <div className={styles.linksContainer}>
+        <a className={styles.navLink} href="">
+          Home
+        </a>
+        <a className={styles.navLink} href="">
+          Our Services
+        </a>
+        <a className={styles.navLink} href="">
+          About Us
+        </a>
+        <a className={styles.navLink} href="">
+          Locations
+        </a>
+      </div>
+      <a className={styles.navBtn} href="">
+        Contact Our Experts
+      </a>
+    </div>
+  );
 }
 
 export default CleaningNav;
