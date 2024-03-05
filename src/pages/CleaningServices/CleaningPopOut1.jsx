f32792d2474e87bdb8e0e88e95aa528e0e10363a
SeunSarumi <seunsarumi@gmail.com>
2024-03-05 18:13:27 +0100

    popout built 90%

--- src/pages/CleaningServices/CleaningPopOut1.jsx
+++ src/pages/CleaningServices/CleaningPopOut1.jsx
@@ -5,8 +5,20 @@ function CleaningPopOut1() {
     <div className={styles.container}>
       {/* div for top text */}
       <div className={styles.topTextContainer}>
-        <h1 className={styles.topText1}> For Inquires</h1>
+        <h1 className={styles.topText1}>&gt; For Inquires</h1>
         <h1 className={styles.topText2}> 07496791064</h1>
+      </div>
+      <div className={styles.bottomTextContainer}>
+        <p>
+          Contact <strong>Angela Okoko</strong> to schedule your complimentary
+          consultation. Whether you seek pristine ironing, deluxe laundry care,
+          or reliable housekeeping and home cleaning services, we're here to
+          meet your needs.
+        </p>
+
+        <strong>Opening Times</strong>
+
+        <p>Mon - Fri: 9am–5pm</p>
       </div>
     </div>
   );
