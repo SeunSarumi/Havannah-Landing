a188b87e002a04f96f78ed0e22f1819ab6141e62
SeunSarumi <seunsarumi@gmail.com>
2024-03-05 18:35:13 +0100

    icon features

--- src/pages/CleaningServices/CleaningMain.jsx
+++ src/pages/CleaningServices/CleaningMain.jsx
@@ -1,11 +1,13 @@
 import CleaningHero from "./CleaningHero";
 import CleaningNav from "./CleaningNav";
+import IconFeatures from "./IconFeatures";
 
 function CleaningMain() {
   return (
     <main>
       <CleaningNav />
       <CleaningHero />
+      <IconFeatures />
     </main>
   );
 }
---
+++ src/pages/CleaningServices/IconFeatures.jsx
@@ -0,0 +1,22 @@
+import styles from "./IconFeatures.module.css";
+
+function IconFeatures() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.featureContainer}>
+        <img src="/images/house-img.png" alt="" />
+        <div className={styles.featureText}>House Cleaning</div>
+      </div>
+      <div className={styles.featureContainer}>
+        <img src="/images/office-img.png" alt="" />
+        <div className={styles.featureText}>Office Cleaning</div>
+      </div>
+      <div className={styles.featureContainer}>
+        <img src="/images/transition-img.png" alt="" />
+        <div className={styles.featureText}>Transition Cleaning</div>
+      </div>
+    </div>
+  );
+}
+
+export default IconFeatures;
