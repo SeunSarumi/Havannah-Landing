31afcecf32ff88a09c6bd65b22e8f0e0c9399bf9
SeunSarumi <seunsarumi@gmail.com>
2024-03-15 20:19:46 +0100

    mobile navigation for homepage

--- src/pages/Homepage/HomeNav.jsx
+++ src/pages/Homepage/HomeNav.jsx
@@ -1,28 +1,65 @@
 import { NavLink } from "react-router-dom";
 import HomeLogo from "./HomeLogo";
-import styles from "./HomeNav.module.css";
+import "./HomeNav.css";
+import { useRef } from "react";
 
 function HomeNav() {
+  const navRef = useRef();
+  const listRef = useRef();
+  const navBtn = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("open-nav");
+    listRef.current.classList.toggle("nav-list-open");
+    navBtn.current.classList.toggle("mobile-close");
+  };
+
   return (
-    <nav className={styles.navBody}>
+    <nav className="navBody">
       <HomeLogo />
-      <div className={styles.linksContainer}>
-        <NavLink className={styles.navLink} href="">
-          Home
-        </NavLink>
-        <NavLink className={styles.navLink} to="/cleaningservices">
-          Our Services
-        </NavLink>
-        <a className={styles.navLink} href="#aboutus">
-          About Us
-        </a>
-        <NavLink className={styles.navLink} href="">
-          Locations
-        </NavLink>
+      <div ref={navRef} className="linksContainer">
+        <ul ref={listRef} className="nav__list ">
+          <li>
+            <NavLink className="navLink" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navLink" to="/cleaningservices">
+              Our Services
+            </NavLink>
+          </li>
+          <li>
+            <a className="navLink" href="#aboutus">
+              About Us
+            </a>
+          </li>
+          <li>
+            <NavLink className="navLink" to="/">
+              Locations
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navBtn">Contact Our Experts</NavLink>
+          </li>
+        </ul>
       </div>
-      <NavLink className={styles.navBtn} href="">
-        Contact Our Experts
-      </NavLink>
+      <button
+        onClick={showNavbar}
+        ref={navBtn}
+        className="mobile-button mobile-open"
+      >
+        <img
+          className="mobile-open-btn"
+          src="/images/menu.svg"
+          alt="burger menu"
+        ></img>
+        <img
+          className="mobile-close-btn"
+          src="/images/close.svg"
+          alt="close menu"
+        ></img>
+      </button>
     </nav>
   );
 }
