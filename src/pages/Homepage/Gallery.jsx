f85fbf3e8bbbe97dbde62860a541a78593be59ba
SeunSarumi <seunsarumi@gmail.com>
2024-03-16 17:07:15 +0100

    new gallery stuff

--- src/pages/Homepage/Gallery.jsx
+++ src/pages/Homepage/Gallery.jsx
@@ -5,7 +5,6 @@ function Gallery() {
     <div className={styles.galleryContainer}>
       <img className={styles.gImage} src="/images/gallery-image-1.png" alt="" />
       <img className={styles.gImage} src="/images/gallery-image-2.png" alt="" />
-      <img className={styles.gImage} src="/images/gallery-image-3.png" alt="" />
     </div>
   );
 }
