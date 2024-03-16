a462ecc392173286048ffe5fca3e4ee99146f415
SeunSarumi <seunsarumi@gmail.com>
2024-03-16 17:11:13 +0100

    carousel response

--- src/pages/Homepage/Carousel.jsx
+++ src/pages/Homepage/Carousel.jsx
@@ -13,6 +13,31 @@ function Carousel() {
     slidesToShow: 3,
     speed: 1000,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <section className={styles.sectionCarousel}>
