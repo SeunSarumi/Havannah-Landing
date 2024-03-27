a8bbdc741611697c42f77f65b5177ab72336f96f
SeunSarumi <seunsarumi@gmail.com>
2024-03-27 16:41:21 +0100

    description test for cleaning services page

--- src/pages/CleaningServices/CleaningMain.jsx
+++ src/pages/CleaningServices/CleaningMain.jsx
@@ -8,6 +8,8 @@ import IconFeatures from "./IconFeatures";
 
 function CleaningMain() {
   document.title = "Havannah Cleaning Services";
+  document.getElementsByTagName("META")[2].content =
+    "Say Goodbye To Clutter!,Book a Session and Embrace simplicity and reclaim your space. Discover the freedom of a clutter-free life with our tailored cleaning solutions ";
   return (
     <div>
       <CleaningNav />
