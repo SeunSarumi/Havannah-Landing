4722d2527f0532fea38528ee7f2c26a44a7299bc
SeunSarumi <seunsarumi@gmail.com>
2026-04-08 01:04:49 +0100

    added countup library for SocialProof

--- src/pages/Version2/SocialProof.jsx
+++ src/pages/Version2/SocialProof.jsx
@@ -1,18 +1,25 @@
 import styles from "./SocialProof.module.css";
+import CountUp from "react-countup";
 
 function SocialProof() {
   return (
     <div className={styles.sectionContainer}>
       <span className={styles.item}>
-        <h1>300+</h1>
+        <h1>
+          <CountUp end={300} duration={1.5} suffix="+" enableScrollSpy />
+        </h1>
         <h2>Properties Cleaned</h2>
       </span>
       <span className={styles.item}>
-        <h1>95%</h1>
+        <h1>
+          <CountUp end={95} duration={1.5} suffix="%" enableScrollSpy />
+        </h1>
         <h2>Client Satisfaction</h2>
       </span>
       <span className={styles.item}>
-        <h1>100%</h1>
+        <h1>
+          <CountUp end={100} duration={1.5} suffix="%" enableScrollSpy />
+        </h1>
         <h2>Trusted by Locals</h2>
       </span>
     </div>
